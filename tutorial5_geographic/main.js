64c0b006824a2aeaf24a5a5f1908ef37a810f151
sethsch <31630735+sethsch@users.noreply.github.com>
2020-04-02 13:01:32 -0400

    updated wiht right Json file path

--- tutorial5_geographic/main.js
+++ tutorial5_geographic/main.js
@@ -44,7 +44,7 @@ let state = {
  * Using a Promise.all([]), we can load more than one dataset at a time
  * */
 Promise.all([
-  d3.json("../data/us-state.json"),
+  d3.json("../data/usState.json"),
   d3.csv("../data/topicComp_byUSstate_2009_19.csv", d3.autoType),
 ]).then(([geojson, NSFdata]) => {
   state.geojson = geojson;
