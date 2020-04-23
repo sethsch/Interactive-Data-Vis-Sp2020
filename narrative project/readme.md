27b38ead6c2b35a926b12cc25790b7fc0bc6b8a0
sethsch <31630735+sethsch@users.noreply.github.com>
2020-04-22 22:22:58 -0400

    added rolling globe package

---
+++ narrative project/readme.md
@@ -0,0 +1,232 @@
+
+
+# Rolling Choropleth Globe in D3
+
+Programmable rolling chloropeth globe in D3 with modifiable country colors, tool-tips and rotations. Pre-compiled to be dropped into your webpage.
+
+Demo: <https://boscoh.github.io/rolling-globe>
+
+## Installation
+
+Download the [package](https://github.com/boscoh/rolling-globe/archive/master.zip).
+
+Open the file `example/index.html` in the browser.
+
+## How-to
+
+_Quick-start._ To create a globe, you need [`require.js`](https://github.com/requirejs/requirejs) and `rolling-globe.min.js` from the `dist` directory in the package. Then in your HTML file:
+
+```html
+<div 
+  id="globe"
+  style="
+    width: calc(100vw - 60px);
+    height: calc(100vh - 160px);">
+</div>
+
+<script src="./require.js"></script>
+
+<script>
+require(['./rolling-globe.min.js'], function (rollingGlobe) {
+    var g = new rollingGlobe.Globe('#globe')
+})
+</script>
+```
+
+_Matching country indices_. To access the the countries in the globe, you need to obtain the index of the country. This can be obtained using the `getICountry` method, with a query based on the built-in country properties (described in the section Looking up Countries below). To find Australia:
+
+```javascript
+var i = g.getICountry({'iso_a3': 'AUS'})
+```
+
+_Rotation to a selected country._
+```javascript
+g.rotateTransitionToICountry(i)
+```
+
+_Handle clicks and double-clicks._
+
+```javascript
+g.clickCountry = function (i) {
+  console.log('clicked: ' + g.features[i].properties.name)
+  g.setHighlight(i)
+  g.rotateTransitionToICountry(i, function () {
+    g.draw()
+  })
+}
+```
+
+_Setting country colors._
+```javascript
+g.colors[i] = 'green'
+g.borderColors[i] = 'blue'
+```
+
+_Setting color palette based on country values._
+```javascript
+// let's say populations is a list of country
+// populations sorted by the country index
+for (var i = 0; i < g.values.length; i += 1) {
+  g.values[i] = populations[i]
+}
+g.resetCountryColorsFromValues('red')
+g.draw()
+
+// optional legend
+g.drawLegend()
+```
+
+_Highlight specific countries with outline._
+```javascript
+g.highlightColor = 'green'
+g.iHighlightCountry = i
+g.draw()
+```
+
+_Replace tool-tip._
+```javascript
+g.getCountryPopupHtml = function (i) {
+  return g.features[i].properties.name + ': ' + g.values[i]
+}
+```
+
+_Resize_: The resize method will resize the globe to fit the parent `<div>`. Attach the function the window resize function:
+```javascript
+window.onresize = function() { g.resize() }
+```
+
+
+## Development
+
+Download package and load the module using require.js
+
+The module has been written in ES6, a very nice dialect, and transpiled into a single file for easy deployment, which includes all the necessary data files to get up and running. To compile `dist/globe.min.js` use:
+
+```bash
+> npm intall
+> webpack
+```
+
+During development, it is suggested to run with the file watcher, and re-load `example/index.html` which uses the compiled version in the `dist` directory:
+
+```bash
+> webpack --watch
+```
+
+### Looking up Countries
+
+Each country is internally represented by an index iCountry, which refers to SVG vector data stored in `this.world`. This index is used to access the `this.values`, `this.colors`, `this.borderColors` and `this.features`.
+
+Each country comes with a default set properties, which is stored in a list `this.features`. Each element in this list `feature` has a sub-field dictionary `feature.properties`, such as this one for Australia:
+
+```
+{
+  "scalerank": 1,
+  "featurecla": "Admin-0 country",
+  "labelrank": 2,
+  "sovereignt": "Australia",
+  "sov_a3": "AU1",
+  "adm0_dif": 1,
+  "level": 2,
+  "type": "Country",
+  "admin": "Australia",
+  "adm0_a3": "AUS",
+  "geou_dif": 0,
+  "geounit": "Australia",
+  "gu_a3": "AUS",
+  "su_dif": 0,
+  "subunit": "Australia",
+  "su_a3": "AUS",
+  "brk_diff": 0,
+  "name": "Australia",
+  "name_long": "Australia",
+  "brk_a3": "AUS",
+  "brk_name": "Australia",
+  "brk_group": "",
+  "abbrev": "Auz.",
+  "postal": "AU",
+  "formal_en": "Commonwealth of Australia",
+  "formal_fr": "",
+  "note_adm0": "",
+  "note_brk": "",
+  "name_sort": "Australia",
+  "name_alt": "",
+  "mapcolor7": 1,
+  "mapcolor8": 2,
+  "mapcolor9": 2,
+  "mapcolor13": 7,
+  "pop_est": 21262641,
+  "gdp_md_est": 800200,
+  "pop_year":` -99,
+  "lastcensus": 2006,
+  "gdp_year":` -99,
+  "economy": "2. Developed region: nonG7",
+  "income_grp": "1. High income: OECD",
+  "wikipedia":` -99,
+  "fips_10": "",
+  "iso_a2": "AU",
+  "iso_a3": "AUS",
+  "iso_n3": "036",
+  "un_a3": 36,
+  "wb_a2": "AU",
+  "wb_a3": "AUS",
+  "woe_id":` -99,
+  "adm0_a3_is": "AUS",
+  "adm0_a3_us": "AUS",
+  "adm0_a3_un":` -99,
+  "adm0_a3_wb":` -99,
+  "continent": "Oceania",
+  "region_un": "Oceania",
+  "subregion": "Australia and New Zealand",
+  "region_wb": "East Asia & Pacific",
+  "name_len": 9,
+  "long_len": 9,
+  "abbrev_len": 4,
+  "tiny":` -99,
+  "homepart": 1
+}
+```
+
+To lookup the iCountry index:
+
+  - you can iterate through the country features `this.features` and match the properties of each country to your country identifier.
+  - or use a default dictionary `this.iCountryFromId` that maps the iCountry index to `iso_n3` field of a country feature.
+  - use the lookup method `this.getICountry({key: value})`, which will match the key, value pair to the values in `feature.properties` of each country, and return the iCountry index if successful
+
+### References
+
+The SVG vector data is stored in `this.world`, and this is taken from Mike Bostocks's [world-atlas](https://github.com/topojson/world-atlas), which was generated from the data at [Natural Earth](http://www.naturalearthdata.com/) at the resolution of 1:110.
+
+The Globe object has the following properties:
+
+ - `this.world` - topoJson data used to generate the SVG
+ - `this.features` - list of features for each country
+ - `this.values` - list of numerical values for each country
+ - `this.colors` - list of colors for countries
+ - `this.borderColors` - list of colors for borders of countries
+ - `this.iCountryFromId` - dictionary to map `iso_n3` to iCountry indice
+ - `this.nullColor` - color of country with values set to null
+ - `this.borderColor` - color of country borders
+ - `this.outerBorderColor` - color of globe border
+ - `this.fillColor` - color of water
+ - `this.highlightColor` - color of border outline for highlighted country
+ - `this.scaleFactor` - zoom factor for globe
+ - `this.iHighlightCountry` - iCountry to be highlighted with this.highlightColor; none if null
+
+Methods:
+
+ - `this.getICountry(query)` - query comes in the form of `{key: value}` that is used to search `this.features` for a matching property, function will return matchin iCountry index, or null
+ - `this.dblclickCountry(iCountry)` - overridable callback for double-click
+ - `this.clickCountry(iCountry)` - overridable callback fro click
+ - `this.getCountryPopupHtml(iCountry)` - overridable callback to generate the HTML for the tool-tip pop-up for a country mouse-over.
+ - `this.resize()` - resizes the globe to the size of the parent div. Useful for web-responsive divs.
+ - `this.rotateTo(r)` - direct rotation to the rotational coordinate `r=[-Longitude, -Latitude]`, this is the internal D3 rotational coordinate system.
+ - `this.rotateTransition(targetR, callback)` - animated transition analogue to `this.rotateTo` with callback upon end of animation
+ - `this.rotateTransitionToICountry(iCountry, callback)` - animated rotation to country
+ - `this.resetCountryColorsFromValues(maxColor, maxValue = null, minColor = '#DDD')` - recolors the countries using `this.values` from `maxColor` to `minColor` using a linear color palette
+ - `this.draw()` - force redraw
+
+
+
+
+
---
+++ rolling-globe-master/src/globe.js
@@ -0,0 +1,469 @@
+import _ from 'lodash'
+import $ from 'jquery'
+import { legendColor } from 'd3-svg-legend'
+const d3 = require('d3')
+const topojson = require('topojson')
+const world110m = require('./data/world-110m.json')
+const world110mInfo = require('./data/world-110m-name.json')
+
+/**
+ * https://stackoverflow.com/questions/2998784/how-to-output-integers-with-leading-zeros-in-javascript
+ */
+function zeroPad(num, places) {
+  let zero = places - num.toString().length + 1
+  return Array(+(zero > 0 && zero)).join('0') + num
+}
+
+/**
+ * A Rolling Chloropeth Globe widget where colors of countries can
+ * be easily remapped and displayed with a legend. Includes
+ * highlight properties.
+ *
+ * Country borders are taken from
+ *   https://github.com/topojson/world-atlas
+ *
+ * Based on the D3 globe code snippets:
+ *  - https://jorin.me/d3-canvas-globe-hover/
+ *  - http://bl.ocks.org/KoGor/5994804
+ *  - http://bl.ocks.org/tlfrd/df1f1f705c7940a6a7c0dca47041fec8
+ *  - https://bl.ocks.org/mbostock/7ea1dde508cec6d2d95306f92642bc42
+ */
+class Globe {
+  /**
+   * @param selector - jquery div tag to insert the globe
+   */
+  constructor(selector, world = world110m, worldData = world110mInfo) {
+    this.world = world
+    this.selector = selector
+    this.scaleFactor = 1 / 2.2
+    this.iHighlightCountry = null
+
+    this.features = topojson.feature(
+      this.world,
+      this.world.objects.countries
+    ).features
+
+    for (let info of worldData) {
+      if (typeof info.iso_n3 === 'number') {
+        info.iso_n3 = zeroPad(info.iso_n3, 3)
+      }
+    }
+
+    // look-up country from numeric ISO country ID
+    this.iCountryFromId = {}
+    for (let i = 0; i < this.features.length; i += 1) {
+      let countryFeature = this.features[i]
+      this.iCountryFromId[countryFeature.id] = i
+      for (let info of worldData) {
+        if (info.iso_n3 === countryFeature.id && info.iso_n3 !== '-99') {
+          _.assign(countryFeature.properties, info)
+          break
+        }
+      }
+    }
+
+    // special data mangling for resolving the two data-sets
+    // in world-110m set from vector and country data
+    let kosovo = worldData[0]
+    let somaliland = worldData[1]
+    let northernCyprus = worldData[2]
+    _.assign(this.features[88].properties, kosovo)
+    _.assign(this.features[38].properties, northernCyprus)
+    _.assign(this.features[145].properties, somaliland)
+
+    let tags = ['adm0_a3', 'gu_a3', 'su_a3', 'brk_a3', 'iso_a3', 'iso_n3', 'adm0_a3_is']
+    for (let i = 0; i < this.features.length; i += 1) {
+      let countryFeature = this.features[i]
+      let strs = _.map(tags, t => countryFeature.properties[t])
+      console.log(i, countryFeature.properties.geounit, strs)
+    }
+
+    this.nullColor = '#CCB'
+    this.borderColor = '#EEE'
+    this.outerBorderColor = '#BBD'
+    this.fillColor = 'aliceblue'
+    this.highlightColor = 'green'
+
+    this.colors = []
+    this.borderColors = []
+    for (let i = 0; i < this.features.length; i += 1) {
+      this.colors.push(this.nullColor)
+      this.borderColors.push(this.borderColor)
+    }
+
+    this.values = []
+    for (let i = 0; i < this.features.length; i += 1) {
+      this.values.push(null)
+    }
+
+    this.pos = { x: 0, y: 0 }
+    this.savePos = { x: 0, y: 0 }
+    this.isMouseDown = false
+
+    this.getCurrentSize()
+
+    this.projection = d3
+      .geoOrthographic()
+      .translate([this.centerX, this.centerY])
+      .scale(this.scale)
+      .clipAngle(90)
+      .precision(0.6)
+
+    this.path = d3.geoPath().projection(this.projection)
+
+    this.svg = d3
+      .select(this.selector)
+      .append('svg')
+      .attr('width', this.width)
+      .attr('height', this.height)
+      .on('mousedown', () => this.mousedown())
+      .on('mouseup', () => this.mouseup())
+      .on('mousemove', () => this.mousemove())
+      .on('mousewheel', () => this.mousewheel())
+      .on('touchstart', () => this.mousedown())
+      .on('touchend', () => this.mouseup())
+      .on('touchmove', () => this.mousemove())
+
+    // draw the fill of sphere
+    this.svg
+      .append('path')
+      .datum({ type: 'Sphere' })
+      .attr('class', 'water')
+      .style('fill', this.fillColor)
+      .style('stroke', 'none')
+      .attr('d', this.path)
+
+    // draw the countries, add change the colors
+    this.svg
+      .selectAll('.country')
+      .data(this.features)
+      .enter()
+      .insert('path')
+      .attr('class', 'country')
+      .attr('d', this.path)
+      .style('stroke', this.borderColor)
+
+    // draw the country outlines
+    this.svg
+      .selectAll('.highlightCountry')
+      .data(this.features)
+      .enter()
+      .insert('path')
+      .attr('class', 'highlightCountry')
+      .attr('d', this.path)
+      .attr('fill', 'none')
+      .style('stroke', 'none')
+
+    // draw the encircling sphere
+    this.svg
+      .append('path')
+      .datum({ type: 'Sphere' })
+      .attr('class', 'water')
+      .style('fill', 'none')
+      .style('stroke', this.outerBorderColor)
+      .attr('d', this.path)
+
+    this.tooltip = d3
+      .select(this.selector)
+      .append('div')
+      .attr('class', 'countryTooltip')
+      .style('display', 'hidden')
+      .style('pointer-events', 'none')
+      .style('position', 'absolute')
+
+    this.svg
+      .selectAll('path.country')
+      .on('mouseover', (d, i) => {
+        let html = this.getCountryPopupHtml(i)
+        if (html) {
+          this.tooltip
+            .html(html)
+            .style('position', 'absolute')
+            .style('display', 'block')
+            .style('opacity', 1)
+          this.moveTooltip()
+        } else {
+          this.tooltip.style('opacity', 0).style('display', 'none')
+        }
+      })
+      .on('mousemove', () => {
+        this.moveTooltip()
+      })
+      .on('mouseout', () => {
+        this.tooltip.style('opacity', 0).style('display', 'none')
+      })
+      .on('dblclick', (d, i) => {
+        this.dblclickCountry(i)
+      })
+      .on('click', (d, i) => {
+        this.clickCountry(i)
+      })
+
+    // build the legend
+    let element = $(this.selector)
+    let elementId = element.attr('id')
+    this.legendId = `${elementId}-legend`
+
+    element.contextmenu(() => false).css({
+      'user-select': 'none',
+      cursor: 'pointer'
+    }).append(`
+        <div
+          style="
+          position: absolute;
+          bottom: 0;
+          padding-left: 10px;
+          user-select: none;
+          pointer-events: none;">
+          <svg id="${this.legendId}"></svg>
+        </div>
+      `)
+
+    this.draw()
+  }
+
+  /**
+   * To be overridden
+   * @param id
+   */
+  dblclickCountry(iCountry) {
+    console.log('> Globe.dblclickCountry', iCountry, d3.event.pageX, d3.event.pageY)
+  }
+
+  /**
+   * To be overridden
+   * @param id
+   */
+  clickCountry(iCountry) {
+    console.log('> Globe.clickCountry', iCountry, d3.event.pageX, d3.event.pageY)
+  }
+
+  moveTooltip() {
+    this.tooltip
+      .style('left', d3.event.pageX + 7 + 'px')
+      .style('top', d3.event.pageY - 15 + 'px')
+  }
+
+  getICountry(query) {
+    for (let i = 0; i < this.features.length; i += 1) {
+      for (let key of _.keys(query)) {
+        if (this.features[i].properties[key] === query[key]) {
+          return i
+        }
+      }
+    }
+    return null
+  }
+
+  /**
+   * To be overridden
+   * @param id
+   * @returns String - contains HTML to write to popup
+   */
+  getCountryPopupHtml(iCountry) {
+    let value = this.values[iCountry]
+    if (value === null) {
+      return ''
+    }
+    return value.toFixed(1)
+  }
+
+  getCurrentSize() {
+    let rect = d3
+      .select(this.selector)
+      .node()
+      .getBoundingClientRect()
+    this.width = rect.width
+    this.height = rect.height
+    this.scale = Math.min(this.width, this.height) * this.scaleFactor
+    this.centerX = this.width / 2
+    this.centerY = this.height / 2
+  }
+
+  resize() {
+    this.getCurrentSize()
+    this.svg.attr('width', this.width).attr('height', this.height)
+    this.projection.translate([this.centerX, this.centerY]).scale(this.scale)
+    this.draw()
+  }
+
+  draw() {
+    this.svg.selectAll('path.water').attr('d', this.path)
+    // draw country fills
+    this.svg
+      .selectAll('path.country')
+      .attr('d', this.path)
+      .style('fill', (d, i) => this.colors[i])
+      .style('stroke', this.borderColor)
+    this.drawHighlight()
+  }
+
+  setHighlight(iCountry) {
+    this.iHighlightCountry = iCountry
+  }
+
+  drawHighlight() {
+    // draw the highlighted country outline
+    this.svg
+      .selectAll('path.highlightCountry')
+      .attr('d', this.path)
+      .style('stroke', (d, i) => {
+        if (i === this.iHighlightCountry) {
+          return this.highlightColor
+        } else {
+          return 'none'
+        }
+      })
+  }
+
+  /**
+   * Rotates globe directly to coordinates in r [-long, -lat]
+   *
+   * @param r - [-Longitude, -Latitude]
+   */
+  rotateTo(r) {
+    if (r[1] < -90) {
+      r[1] = -90
+    }
+    if (r[1] > 90) {
+      r[1] = 90
+    }
+    this.projection.rotate(r)
+    this.draw()
+  }
+
+  /**
+   * Animated transition to this.rotateTo with callback when
+   * the animation is finished
+   *
+   * @param targetR - [-Longitude, -Latitude]
+   * @param callback
+   */
+  rotateTransition(targetR, callback) {
+    let interpolateR = d3.interpolate(this.projection.rotate(), targetR)
+    let rotate = t => {
+      this.rotateTo(interpolateR(t))
+    }
+    d3.transition()
+      .duration(1250)
+      .tween('rotate', () => rotate)
+      .on('end', callback)
+  }
+
+  rotateTransitionToICountry(iCountry, callback) {
+    let selectedFeature = this.features[iCountry]
+    let p = d3.geoCentroid(selectedFeature)
+    this.rotateTransition([-p[0], -p[1]], callback)
+  }
+
+  rotateRel(diffR) {
+    let r = this.projection.rotate()
+    r[0] += diffR[0]
+    r[1] += diffR[1]
+    this.rotateTo(r)
+  }
+
+  resetCountryColorsFromValues(maxColor, maxValue = null, minColor = '#DDD') {
+    if (maxValue === null) {
+      maxValue = Math.max.apply(null, this.values)
+    }
+    this.paletteScale = d3
+      .scaleLinear()
+      .domain([0, maxValue])
+      .range([minColor, maxColor])
+
+    for (let i = 0; i < this.features.length; i += 1) {
+      if (this.values[i] == null) {
+        this.colors[i] = this.nullColor
+      } else {
+        this.colors[i] = this.paletteScale(this.values[i])
+      }
+    }
+  }
+
+  extractPointerPos() {
+    let event = d3.event
+    event.preventDefault()
+    let x, y
+
+    if (event.changedTouches) {
+      x = event.changedTouches[0].pageX
+      y = event.changedTouches[0].pageY
+    } else if (event.touches) {
+      x = event.touches[0].clientX
+      y = event.touches[0].clientY
+    } else {
+      x = event.clientX
+      y = event.clientY
+    }
+
+    let rect = d3
+      .select(this.selector)
+      .node()
+      .getBoundingClientRect()
+
+    x +=
+      document.body.scrollLeft + document.documentElement.scrollLeft - rect.left
+    y += document.body.scrollTop + document.documentElement.scrollTop - rect.top
+
+    this.pos.x = x
+    this.pos.y = y
+  }
+
+  mousemove() {
+    this.extractPointerPos()
+    if (this.isMouseDown) {
+      this.rotateRel([
+        +0.3 * (this.pos.x - this.savePos.x),
+        -0.3 * (this.pos.y - this.savePos.y)
+      ])
+      this.savePos = _.clone(this.pos)
+    }
+  }
+
+  mousedown() {
+    this.mousemove()
+    this.savePos = _.clone(this.pos)
+    this.isMouseDown = true
+  }
+
+  mouseup() {
+    this.mousemove()
+    this.isMouseDown = false
+  }
+
+  mousewheel() {
+    // event.preventDefault()
+
+    let wheel
+    let e = d3.event
+    if (e.wheelDelta) {
+      wheel = e.wheelDelta / 3000
+    } else {
+      // for Firefox
+      wheel = -e.detail / 300
+    }
+
+    this.scaleFactor = this.scaleFactor * (1 - wheel)
+    if (this.scaleFactor < 0.1) {
+      this.scaleFactor = 0.1
+    }
+
+    this.resize()
+  }
+
+  drawLegend() {
+    let svg = d3.select(`#${this.legendId}`)
+    svg.html('')
+    let colorLegend = legendColor()
+      .labelFormat(d3.format('.0f'))
+      .scale(this.paletteScale)
+      .shapePadding(0)
+      .shapeWidth(20)
+      .shapeHeight(20)
+      .labelOffset(8)
+    svg.append('g').call(colorLegend)
+  }
+}
+
+export { Globe }
